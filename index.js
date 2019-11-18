a779713dea43bf456fc10fc78445f51026e8d7ad
vtranduc <vtranduc@ryerson.ca>
2019-11-18 17:01:16 -0500

    initial setup of background

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View, VrButton } from "react-360";
+import house from "./data/houseData";
 
 export default class HouseTourVR extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
+          <Text style={styles.greeting}>Room Selection</Text>
+          <Text>{house.House.roomName}</Text>
+        </View>
+
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>Room</Text>
+          <Text>{house.House.info}</Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
+    borderWidth: 2
   },
   greeting: {
-    fontSize: 30,
-  },
+    fontSize: 30
+  }
 });
 
-AppRegistry.registerComponent('HouseTourVR', () => HouseTourVR);
+AppRegistry.registerComponent("HouseTourVR", () => HouseTourVR);
